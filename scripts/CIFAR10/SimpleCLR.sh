ff9239a7b8a3fa47fe34ee1cd45129e2dd541872
Huanran Li <nemo0421@gmail.com>
2024-02-13 23:10:14 -0600

    Update SimpleCLR.sh

--- scripts/CIFAR10/SimpleCLR.sh
+++ scripts/CIFAR10/SimpleCLR.sh
@@ -1,6 +1,6 @@
 cd ../src/
 
-python main.py --max_epochs 100 --experiment_name CIFAR10 --run_index $1
+python main.py --max_epochs 10 --experiment_name CIFAR10 --run_index $1
 
 cd ../scripts
 ./gitupdate.sh SimpleTest
