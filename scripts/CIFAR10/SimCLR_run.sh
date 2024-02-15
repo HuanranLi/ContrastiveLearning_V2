4a9d44672f4f10a8894235dcee6bd9913af7573a
Huanran Li <nemo0421@gmail.com>
2024-02-14 23:39:27 -0600

    update

--- scripts/CIFAR10/SimCLR_run.sh
+++ scripts/CIFAR10/SimCLR_run.sh
@@ -1,6 +1,6 @@
 cd ../src/
 
-python main.py --max_epochs 100 --experiment_name CIFAR10_SimCLR_$1 --run_index $1
+python main.py --max_epochs 500 --experiment_name CIFAR10_SimCLR_$1 --run_index $1
 
 cd ../scripts
 ./git_log_update.sh SimpleTest
