f2e41390dad973b74bd20fb2433f0fc80617dd97
Huanran Li <nemo0421@gmail.com>
2024-02-14 23:38:50 -0600

    update

--- scripts/CIFAR10/test.sh
+++ scripts/CIFAR10/test.sh
@@ -1,5 +1,5 @@
 cd ../src/
 
-python SSL_main.py --max_epochs 15 --experiment_name test --accelerator mps
+python SSL_main.py --max_epochs 2 --experiment_name test --accelerator mps
 
 cd ../scripts
