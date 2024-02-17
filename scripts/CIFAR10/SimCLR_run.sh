36002143509657d3ded9f8026fa87aca071358bf
Huanran Li <nemo0421@gmail.com>
2024-02-17 11:12:46 -0600

    no more git backup

--- scripts/CIFAR10/SimCLR_run.sh
+++ scripts/CIFAR10/SimCLR_run.sh
@@ -3,4 +3,4 @@ cd ../src/
 python SSL_main.py --max_epochs 500 --experiment_name CIFAR10_SimCLR_$1 --run_index $1
 
 cd ../scripts
-./git_log_update.sh SimpleTest
+# ./git_log_update.sh SimpleTest
--- scripts/CIFAR10/SimCLR_smTemp_run.sh
+++ scripts/CIFAR10/SimCLR_smTemp_run.sh
@@ -3,4 +3,4 @@ cd ../src/
 python SSL_main.py --max_epochs 500 --experiment_name CIFAR10_SimCLR_SmTemp_$1 --run_index $1 --LossTemperature 0.1
 
 cd ../scripts
-./git_log_update.sh InfoNCE Small temperature run
+# ./git_log_update.sh InfoNCE Small temperature run
