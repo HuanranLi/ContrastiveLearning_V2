af2020394ff4caaea601c90542d8496a57afb05b
Huanran Li <nemo0421@gmail.com>
2024-02-23 11:07:52 -0600

    Update CHTC_TransFusion_Info_MidTemp_run.sh

--- scripts/CIFAR10/CHTC_TransFusion_Info_MidTemp_run.sh
+++ scripts/CIFAR10/CHTC_TransFusion_Info_MidTemp_run.sh
@@ -1,5 +1,4 @@
 cd ../src/
 
-python SSL_main.py --run_name TF5_Info_ITER1000 --num_TF_layers 5 --experiment_name TF1_Info_ITER1000 --max_epochs 3000 --LossTemperature 0.5 --projection_head TransFusion --learning_rate 1e-2
-
+python SSL_main.py --run_name TF5_Info_ITER1000 --num_TF_layers 5 --experiment_name TF1_Info_ITER1000 --max_epochs 3000 --LossTemperature 0.5 --projection_head TransFusion
 cd ../scripts
