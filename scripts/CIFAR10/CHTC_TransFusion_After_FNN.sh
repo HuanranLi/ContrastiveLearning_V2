1023c3a0eb560a9bb26b64f92207e696baac7e09
Huanran Li <nemo0421@gmail.com>
2024-03-05 19:04:18 -0600

    1

--- scripts/CIFAR10/CHTC_TransFusion_After_FNN.sh
+++ scripts/CIFAR10/CHTC_TransFusion_After_FNN.sh
@@ -1,5 +1,5 @@
 cd ../src/
 
-python SSL_main.py --projection_head TransFusion-after_FNN --num_TF_layers 5 --experiment_name TransFusion_After_FNN_1K --run_name TransFusion_After_FNN_1K --max_epochs 1000 --LossTemperature 0.5
+python SSL_main.py --criterion SymNSQ --learning_rate 1e-1 --projection_head TransFusion-after_FNN --num_TF_layers 5 --experiment_name TFSymNSQ_After_FNN_1K --run_name TFSymNSQ_After_FNN_1K --max_epochs 1000 --LossTemperature 0.5
 
 cd ../scripts
--- scripts/CIFAR10/TransFusion_test_run.sh
+++ scripts/CIFAR10/TransFusion_test_run.sh
@@ -4,14 +4,16 @@ cd ../src/
 # python SSL_main.py --run_name 5_Layer_TF_Info --num_TF_layers 5 --experiment_name TransFusion_MidTemp_ITER30 --max_epochs 15 --accelerator mps --LossTemperature 0.5 --projection_head TransFusion
 # python SSL_main.py --run_name Info_bench --num_TF_layers 5 --experiment_name TransFusion_MidTemp_ITER30 --max_epochs 15 --accelerator mps --LossTemperature 0.5
 
-learning_rate=1e-5
-projection_head=TransFusionV2
+learning_rate=1
+projection_head=TransFusion-after_FNN
 num_TF_layers=5
+criterion=SymNSQ
 
-run_name=SymNSQ_${projection_head}_Layer${num_TF_layers}_LR${learning_rate}
+run_name={$criterion}_${projection_head}_Layer${num_TF_layers}_LR${learning_rate}
 
 
-python SSL_main.py --criterion SymNSQ --learning_rate $learning_rate --run_name $run_name --projection_head $projection_head --num_TF_layers $num_TF_layers --experiment_name TransFusion_MidTemp_ITER30 --max_epochs 5 --accelerator mps --LossTemperature 0.5
+python SSL_main.py --criterion $criterion --learning_rate $learning_rate --run_name $run_name --projection_head $projection_head --num_TF_layers $num_TF_layers --experiment_name TransFusion_Test --max_epochs 15 --accelerator mps --LossTemperature 0.5
 
+#--
 
 cd ../scripts
